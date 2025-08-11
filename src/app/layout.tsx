d087ab2192060c28cda4a820d08c45d107826136
Harry Cheng <harryc3015@gmail.com>
2025-08-11 00:30:34 -0400

    fixing icon location

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/icons/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
